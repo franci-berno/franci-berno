2b57b052824ff13bf0695232d2c5979f41cdaa63
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-03-13 23:54:19 +0100

    update

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,8 @@
-import { getPage } from '$lib/api';
-
 export async function load({ locals }) {
   const currentUser = locals.user;
-  // const articles = await getArticles();
-  const page = await getPage('home');
 
   return {
     currentUser,
-    // articles: articles.slice(0, 3),
-    page
+    page: null
   };
 }
