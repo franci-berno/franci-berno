888b046ef48997ec87fa64d7a8ede032599d919b
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-03-13 23:31:08 +0100

    hendleHttpError

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,17 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // ignore deliberate link to shiny 404 page
+        if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+          return;
+        }
+
+        // otherwise fail the build
+        throw new Error(message);
+      }
+    },
     adapter: adapter(),
     paths: {
       base: dev ? '' : process.env.BASE_PATH
