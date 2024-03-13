e74f6a945f28600d707c067da85fe260d5f6384b
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-03-13 19:34:37 +0100

    Initial commit

---
+++ src/routes/+page.server.js
@@ -0,0 +1,13 @@
+import { getPage } from '$lib/api';
+
+export async function load({ locals }) {
+  const currentUser = locals.user;
+  // const articles = await getArticles();
+  const page = await getPage('home');
+
+  return {
+    currentUser,
+    // articles: articles.slice(0, 3),
+    page
+  };
+}
---
+++ src/routes/data.ts
@@ -0,0 +1,26 @@
+const testimonals: {
+  text: string;
+  image: string;
+  name: string;
+} = {
+  text: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. ”',
+  image: '/images/person-placeholder.jpg',
+  name: 'Jane Doe · jane-doe.org'
+};
+
+const faqs: string = `
+  <h2>Domanda 1</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi lectus, pellentesque nec urna eget, pretium dictum arcu. In rutrum pretium leo, id efficitur nisl ullamcorper sit amet.</p>
+  <h2>Domanda 2</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi lectus, pellentesque nec urna eget, pretium dictum arcu. In rutrum pretium leo, id efficitur nisl ullamcorper sit amet.</p>
+`;
+
+const placeholder: string = `
+		<p>Modern tools, such as Svelte and Tailwind allow you to easily hand-craft fast and beautiful websites. What’s missing is the ability to <strong>make edits without changing the source code</strong>.</p>
+		<p>With this <a href="https://github.com/michael/editable-website">open-source website template</a>, I want to fill that gap.</p>
+    <p>If you have questions or need any help, contact me.</p>
+	`;
+
+const title: string = 'Francesco Bernini';
+
+export let dati = { testimonals, faqs, placeholder, title };
